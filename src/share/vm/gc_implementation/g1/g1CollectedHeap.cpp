fc205d6272b4a99f502a6e2e1cfa05bab31260ae
jwilhelm <jwilhelm@localhost>
2014-03-24 06:55:00 -0700

    Merge

--- src/share/vm/gc_implementation/g1/g1CollectedHeap.cpp
+++ src/share/vm/gc_implementation/g1/g1CollectedHeap.cpp
@@ -2265,7 +2265,7 @@ void G1CollectedHeap::ref_processing_init() {
                                 // (for efficiency/performance)
                            false);
                                 // Setting next fields of discovered
-                                // lists requires a barrier.
+                                // lists does not require a barrier.
 }
 
 size_t G1CollectedHeap::capacity() const {
@@ -4743,6 +4743,12 @@ oop G1ParScanThreadState::copy_to_survivor_space(oop const old) {
   oop forward_ptr = old->forward_to_atomic(obj);
   if (forward_ptr == NULL) {
     Copy::aligned_disjoint_words((HeapWord*) old, obj_ptr, word_sz);
+
+    // alloc_purpose is just a hint to allocate() above, recheck the type of region
+    // we actually allocated from and update alloc_purpose accordingly
+    HeapRegion* to_region = _g1h->heap_region_containing_raw(obj_ptr);
+    alloc_purpose = to_region->is_young() ? GCAllocForSurvived : GCAllocForTenured;
+
     if (g1p->track_object_age(alloc_purpose)) {
       // We could simply do obj->incr_age(). However, this causes a
       // performance issue. obj->incr_age() will first check whether
